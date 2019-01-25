ae1922fc08347acac02cf18fc1eac4a51a48b28b
Dayton <dayton@Daytons-MacBook-Pro.local>
2019-01-25 14:47:30 -0600

    added expenses

---
+++ Expenses/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expenses
+//
+//  Created by Dayton on 1/25/19.
+//  Copyright © 2019 Dayton Steffeny. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title: String
+    let amount: Double
+    let date: Date
+}
---
+++ Expenses/ExpenseTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ExpenseTableViewCell.swift
+//  Expenses
+//
+//  Created by Dayton on 1/25/19.
+//  Copyright © 2019 Dayton Steffeny. All rights reserved.
+//
+
+import UIKit
+
+class ExpenseTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
